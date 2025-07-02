a06b7a6132cceb0d06117aae033417d2fb9ef719
usbmkt <usbmktpro@gmail.com>
2025-07-01 23:58:49 -0300

    Update user.py

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
-from src.models.user import User, db
+# CORREÇÃO: A importação agora é relativa ao diretório 'src'
+from models.user import User, db
 
 user_bp = Blueprint('user', __name__)
 
@@ -10,8 +11,10 @@ def get_users():
 
 @user_bp.route('/users', methods=['POST'])
 def create_user():
-    
     data = request.json
+    if not data or not data.get('username') or not data.get('email'):
+        return jsonify({'error': 'username e email são obrigatórios'}), 400
+        
     user = User(username=data['username'], email=data['email'])
     db.session.add(user)
     db.session.commit()
